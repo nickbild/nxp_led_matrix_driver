f79671d118b708cbe0c522d3643b2d22dc14f948
nickbild <Nicholas.Bild@sprint.com>
2019-12-30 12:10:59 -0500

    Update data generator and add some graphics.

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -2,14 +2,19 @@ import re
 import sys
 
 
+image_change_speed = 6000  # Higher value = slower animation speed.
+
 totalFrames = 40
-denominator = 256 / totalFrames	# Scale colors to fit in PWM space.
+denominator = 256 / totalFrames	 # Scale colors to fit in PWM space.
+num_images = 0
 
 red_all = []
 green_all = []
 blue_all = []
 
 for file in sys.argv[1:]:
+    num_images += 1
+
     red_frame = []
     green_frame = []
     blue_frame = []
@@ -46,16 +51,17 @@ for file in sys.argv[1:]:
     green_all.append(green_frame)
     blue_all.append(blue_frame)
 
+
+change_image_frames = -1
+if num_images > 1:
+    change_image_frames = image_change_speed
+
+
 print("\n*** Copy/paste below data structures into 'Data to Display' section of source code. ***\n\n")
 
-print("__RODATA(Flash2) const unsigned short int brightnessR[numImages][32][32] = ")
-print(str(red_all).replace('[','{').replace(']','}'))
-print(";")
+print("#define numImages {}  // >1 means animation.".format(num_images))
+print("#define changeImageFrames {}  // Animation control. Set to -1 to display a single image.  Larger number == longer delay between image changes.\n".format(change_image_frames))
 
-print("__RODATA(Flash2) const unsigned short int brightnessG[numImages][32][32] = ")
-print(str(green_all).replace('[','{').replace(']','}'))
-print(";")
-
-print("__RODATA(Flash2) const unsigned short int brightnessB[numImages][32][32] = ")
-print(str(blue_all).replace('[','{').replace(']','}'))
-print(";")
+print("__RODATA(Flash2) const unsigned short int brightnessR[numImages][32][32] = {};\n".format(str(red_all).replace('[','{').replace(']','}')))
+print("__RODATA(Flash2) const unsigned short int brightnessG[numImages][32][32] = {};\n".format(str(green_all).replace('[','{').replace(']','}')))
+print("__RODATA(Flash2) const unsigned short int brightnessB[numImages][32][32] = {};\n".format(str(blue_all).replace('[','{').replace(']','}')))
